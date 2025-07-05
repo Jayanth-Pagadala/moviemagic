81d6b6a143836e9ce789fcf36ef47296ece71bbd
Jayanth-Pagadala <jayanthpagadala05@gmail.com>
2025-07-05 17:30:07 +0530

    app.py

--- flask_static_test/flask_static_test/app.py
+++ flask_static_test/flask_static_test/app.py
@@ -8,7 +8,7 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1') # e.g., 'ap-south
 users_table = dynamodb.Table("MovieMagic_Users")
 bookings_table = dynamodb.Table("MovieMagic_Bookings")
 
-sns = boto3.client('sns', region_name="us-east-1") sns_topic_arn = 'arn:aws:sns:us-east-1:971422691207:Movie:abbc9a87-352a-4295-9ffe-7bf675a077b5'
+sns = boto3.client('sns', region_name="us-east-1") sns_topic_arn = 'arn:aws:sns:us-east-1:971422691207:Movie'
 
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
